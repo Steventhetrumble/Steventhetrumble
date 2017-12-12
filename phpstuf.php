7cb265b0624ba5afed8f946297500b0f73f3dc25
Steven Trumble <steven.trumble@dcmail.ca>
2017-12-12 16:56:00 -0500

    trying

---
+++ phpstuf.php
@@ -0,0 +1,16 @@
+<?php
+if ($_POST['user_name']){
+  print("Welcome, ");
+  print($_POST['user_name']);
+}
+else{
+  print <<< _HTML_
+  <FORM method="post" action = "$_SERVER[PHP_SELF]">
+  Enter your name: <input type="text" name="user_name">
+  <BR/>
+  <INPUT type="submit" value="SUBMIT NAME">
+  </FORM>
+  _HTML_;
+}
+
+?>
