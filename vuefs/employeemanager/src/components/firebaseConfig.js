fcb0dfbf71312577f5fb5a76e6237e689de0cce4
StevenAndrew Trumble <StevenAndrew.Trumble@rci.rogers.ca>
2018-05-15 10:17:40 -0400

    Scheduled Push

---
+++ vuefs/employeemanager/src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+  apiKey: "AIzaSyD0ekU1HSm9SJ4aas7Nh31jqjJVyvqsF-Y",
+  authDomain: "vuefs-prod-69b03.firebaseapp.com",
+  databaseURL: "https://vuefs-prod-69b03.firebaseio.com",
+  projectId: "vuefs-prod-69b03",
+  storageBucket: "vuefs-prod-69b03.appspot.com",
+  messagingSenderId: "301986680100"
+}
