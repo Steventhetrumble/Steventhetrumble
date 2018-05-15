c01b5f317bfbfdf77c931aa7c396894b4e73bb08
StevenAndrew Trumble <StevenAndrew.Trumble@rci.rogers.ca>
2018-05-15 16:11:58 -0400

    part4

--- vuefs/employeemanager/src/components/firebaseinit.js
+++ vuefs/employeemanager/src/components/firebaseinit.js
@@ -2,5 +2,6 @@ import firebase from 'firebase'
 import 'firebase/firestore'
 import firebaseConfig from './firebaseConfig'
 const firebaseApp = firebase.initializeApp
+
 (firebaseConfig)
 export default firebaseApp.firestore()
